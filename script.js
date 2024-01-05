73b49aa4091d10d2d0d0a4ec8418cd51796b30b9
Sopu Matei <sopumatei@gmail.com>
2024-01-05 02:33:36 +0200

    Add check password function
    Now, the password from the password input must
    match the confirm password input or you wouldn't
    be able to send the form

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('confirm-password');
+const createAccBtn = document.getElementById('create-acc-btn');
 
+function checkPassword() {
+    if(passwordInput.value) {
+        if(passwordInput.value === confirmPasswordInput.value){
+            passwordInput.style.borderColor = 'green';
+            confirmPasswordInput.style.borderColor = 'green';
+            createAccBtn.setAttribute('type', 'submit');
+        }
+        else {
+            passwordInput.style.borderColor = 'red';
+            confirmPasswordInput.style.borderColor = 'red';
+            createAccBtn.setAttribute('type', 'button');
+        }
+    } 
+    else {
+        passwordInput.style.borderColor = 'black';
+        confirmPasswordInput.style.borderColor = 'black';
+    }  
+}
+
+setInterval(checkPassword, 200);
